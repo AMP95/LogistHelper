1e51aa5ecc72c7c72a8e36758c9632aa836cf1c2
AMP95 <aikosan95@gmail.com>
2025-01-08 23:32:58 +0700

    add templated list and edit view

---
+++ LogistHelper/UI/Views/Base/EditView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LogistHelper.UI.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для EditView.xaml
+    /// </summary>
+    public partial class EditView : UserControl
+    {
+        public EditView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LogistHelper/UI/Views/Base/ListView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LogistHelper.UI.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ListView.xaml
+    /// </summary>
+    public partial class ListView : UserControl
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
