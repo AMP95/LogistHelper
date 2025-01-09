61c9db8bd75d7804932e20ef06f784430913850f
amp <mpakimova95@gmail.com>
2025-01-09 09:40:51 +0700

    add custom pwd

--- LogistHelper/UI/CustomControls/CustomPasswordBox.xaml.cs
+++ LogistHelper/UI/CustomControls/CustomPasswordBox.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LogistHelper.UI.CustomControls
 {
@@ -20,9 +8,87 @@ namespace LogistHelper.UI.CustomControls
     /// </summary>
     public partial class CustomPasswordBox : UserControl
     {
+        public PasswordBox PasswordBox { get; private set; }
+
+        public Style PasswordBoxStyle
+        {
+            get { return (Style)GetValue(PasswordBoxStyleProperty); }
+            set { SetValue(PasswordBoxStyleProperty, value); }
+        }
+        public static readonly DependencyProperty PasswordBoxStyleProperty =
+            DependencyProperty.Register("PasswordBoxStyle", typeof(Style), typeof(CustomPasswordBox));
+
+
+        public Style ShowedPasswordBoxStyle
+        {
+            get { return (Style)GetValue(ShowedPasswordBoxStyleProperty); }
+            set { SetValue(ShowedPasswordBoxStyleProperty, value); }
+        }
+        public static readonly DependencyProperty ShowedPasswordBoxStyleProperty =
+            DependencyProperty.Register("ShowedPasswordBoxStyle", typeof(Style), typeof(CustomPasswordBox));
+
+
+        public Style ShowButtonStyle
+        {
+            get { return (Style)GetValue(ShowButtonStyleProperty); }
+            set { SetValue(ShowButtonStyleProperty, value); }
+        }
+        public static readonly DependencyProperty ShowButtonStyleProperty =
+            DependencyProperty.Register("ShowButtonStyle", typeof(Style), typeof(Style));
+
+        public bool IsPasswordShowed
+        {
+            get { return (bool)GetValue(IsPasswordShowedProperty); }
+            set { SetValue(IsPasswordShowedProperty, value); }
+        }
+        public static readonly DependencyProperty IsPasswordShowedProperty =
+            DependencyProperty.Register("IsPasswordShowed", typeof(bool), typeof(CustomPasswordBox),
+                new FrameworkPropertyMetadata(new PropertyChangedCallback((d, e) =>
+                {
+                    if (d is CustomPasswordBox custom)
+                    {
+                        if (custom.IsPasswordShowed)
+                        {
+                            custom.pwdBox.Visibility = Visibility.Hidden;
+                            custom.showedPwd.Visibility = Visibility.Visible;
+                            custom.showedPwd.Focus();
+                        }
+                        else
+                        {
+                            custom.pwdBox.Visibility = Visibility.Visible;
+                            custom.showedPwd.Visibility = Visibility.Hidden;
+                            custom.pwdBox.Focus();
+                        }
+                    }
+                })));
+
+
         public CustomPasswordBox()
         {
             InitializeComponent();
+            PasswordBox = pwdBox;
+        }
+
+        private void pwdBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            ChangePassword(pwdBox.Password);
+        }
+
+        private void showedPwd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ChangePassword(showedPwd.Text);
+        }
+
+        private void ChangePassword(string password)
+        {
+            if (!pwdBox.Password.Equals(password))
+            {
+                pwdBox.Password = password;
+            }
+            if (!showedPwd.Text.Equals(password))
+            {
+                showedPwd.Text = password;
+            }
         }
     }
 }
